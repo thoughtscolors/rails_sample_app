c0541851e9e8b93f8d048d0d9fd63f4431e892e0
Brady <vanderbannond@gmail.com>
2018-12-13 10:52:44 -0800

    static page routes rendering, added comments in ctrlr

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
+  # < means staticpagescontroller inherits from applicationcontroller class
+  # when visiting the URL /static_pages/home, Rails looks in the Static Pages controller and executes the code in the home action, and then renders the view corresponding to the action. 
   def home
   end
 
