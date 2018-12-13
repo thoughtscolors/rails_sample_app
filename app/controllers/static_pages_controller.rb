045de6ed9a2860ed75834e140f4629fd51f9880e
Brady <vanderbannond@gmail.com>
2018-12-13 11:25:59 -0800

    added about test and route, passing test

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,13 @@
 class StaticPagesController < ApplicationController
   # < means staticpagescontroller inherits from applicationcontroller class
-  # when visiting the URL /static_pages/home, Rails looks in the Static Pages controller and executes the code in the home action, and then renders the view corresponding to the action. 
+  # when visiting the URL /static_pages/home, Rails looks in the Static Pages controller and executes the code in the home action, and then renders the view corresponding to the action.
   def home
   end
 
   def help
   end
+
+  def about
+  end
+  
 end
