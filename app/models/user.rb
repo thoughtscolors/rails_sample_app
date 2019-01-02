4cdf510af76d2ab97cf62121cfc3b4fba862ce86
Brady <vanderbannond@gmail.com>
2019-01-02 15:24:23 -0800

    add user cookies and token digest, failing logout test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
   # can also be written as
   # before_save { self.email = self.email.downcase } <- right hand side self is optional.
@@ -9,7 +11,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
   format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  
+
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
@@ -17,5 +19,18 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
