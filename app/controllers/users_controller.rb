fc2f0f8ffdbef50b010dc3c7b9db87baa06fa2be
Brady <vanderbannond@gmail.com>
2019-01-01 20:04:08 -0800

    display user avatar, changed seed email to railstutorial.org

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
 
   def new
-    
+
   end
 
   def show
     @user = User.find(params[:id])
+    # debugger
+    # uses byebug gem, press Ctrl-D to step forward, acts as console /w breakpoints
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
-user = User.new(name: "User 1", email: "user1@user.com", password: "secret123", password_confirmation: "secret123")
+user = User.new(name: "User 1", email: "example@railstutorial.org", password: "secret123", password_confirmation: "secret123")
 user.save
