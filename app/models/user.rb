465f4d77b13d77306f717c251d3bb2161d2c17b0
Brady <vanderbannond@gmail.com>
2019-01-01 18:47:39 -0800

    add indexing to user email and db unique and downcase, passing tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  # can also be written as
+  # before_save { self.email = self.email.downcase } <- right hand side self is optional.
+  # can also be written as
+  # before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
