bdac5193cd9df23fa52e8d35160cf3072b3cdea7
Brady <vanderbannond@gmail.com>
2019-01-01 18:09:52 -0800

    add user model tests, passing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "User 1", email: "user1@user.com")
+  end
+
+  test 'user should be valid' do
+    assert @user.valid?
+  end
 end
