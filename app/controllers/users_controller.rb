d79b1b609a27b02ac8bc0c9c7911d86dbe9f1f47
Brady <vanderbannond@gmail.com>
2019-01-01 20:46:09 -0800

    add redirect on successful user create, working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-
+      redirect_to @user
+      # can also be writted as
+      # redirect_to user_url(@user)
     else
       render 'new'
     end
