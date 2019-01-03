9aacd0fdbdaf4fe06e8603e1afa93ab47544ead0
Brady <vanderbannond@gmail.com>
2019-01-02 22:26:53 -0800

    update seeds with account activations

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
-User.create!(name: "User 1", email: "example@railstutorial.org", password: "password", password_confirmation: "password", admin: true)
+User.create!( name: "User 1",
+              email: "example@railstutorial.org",
+              password: "password",
+              password_confirmation: "password",
+              admin: true,
+              activated: true,
+              activated_at: Time.zone.now )
 
 99.times do |n|
   name  = Faker::Name.name
@@ -15,5 +21,7 @@ User.create!(name: "User 1", email: "example@railstutorial.org", password: "pass
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
