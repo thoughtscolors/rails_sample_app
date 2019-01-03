b036057fda01ff53cc579c73b1d474ca8fc52c35
Brady <vanderbannond@gmail.com>
2019-01-02 20:14:27 -0800

    add faker db seeds and refactor initial seed to throw error

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
-user = User.new(name: "User 1", email: "example@railstutorial.org", password: "secret123", password_confirmation: "secret123")
-user.save
+user = User.create!(name: "User 1", email: "example@railstutorial.org", password: "secret123", password_confirmation: "secret123")
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
