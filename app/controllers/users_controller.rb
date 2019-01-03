da7cba72af867c0e740f84998fc95fce6da77731
Brady <vanderbannond@gmail.com>
2019-01-02 21:10:00 -0800

    add user destroy for admin and testing, passing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -43,6 +44,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    user = User.find(params[:id]).destroy
+    flash[:success] = "User #{user.name} deleted"
+    redirect_to users_url
+  end
+
   # Before filters
   # Confirms a logged-in user.
   def logged_in_user
@@ -62,5 +69,10 @@ class UsersController < ApplicationController
     private
       def user_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
+      
+      # Confirms an admin user.
+      def admin_user
+        redirect_to(root_url) unless current_user.admin?
       end
 end
