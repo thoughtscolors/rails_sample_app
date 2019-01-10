1b62d55e9447125a56f48f7f05486f79c95e75a4
Brady <vanderbannond@gmail.com>
2019-01-10 14:02:32 -0800

    add microposts routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :microposts, only: [:create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
